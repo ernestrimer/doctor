0d8d22a7cdb357066c7fc0eedb74d54bab4efee8
Ernie Rimer <ernest.rimer@icloud.com>
2020-06-10 22:25:45 -0600

    touch up syntax

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -5,4 +5,8 @@ class Doc < ApplicationRecord
   validates :dr_name, :dr_num, presence: true
   validates :dr_num, uniqueness: true
   validates :dr_name, :dr_num, length: { minimum: 1 }
+
+  def full_name
+    "#{self.dr_name} (aka: Dr. #{self.dr_num})"
+  end
 end
